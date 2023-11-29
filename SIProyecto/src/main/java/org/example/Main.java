4894eb7d7c4514d36d1f0ede3b1b0b4544b18a18
pepinadas <mmmunapregunta@gmail.com>
2023-11-29 14:49:16 -0600

    Agregando el Jar

--- SIProyecto/src/main/java/org/example/Main.java
+++ SIProyecto/src/main/java/org/example/Main.java
@@ -9,7 +9,15 @@ public class Main {
         System.out.println("Bienvenido al conversor de numeros de 32 bits a redes IPv4");
         System.out.println("Por favor introduzca el numero a continuacion");
 
-        long numero = scanner.nextLong();
+        long numero;
+        long limite = 214748364;
+        do {
+            numero = scanner.nextLong();
+            if (numero < limite) {
+                System.out.println("Colocar un numero valido");
+            }
+        } while (numero < limite);
+
 
 
         System.out.println("Tu direccion IPv4 es: " + LongToIp.longToIP(numero));
